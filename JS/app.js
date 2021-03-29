db3a942351c432e1d08c58b233137109dc1166ac
Hellemond Dargalaii <hdargalaii@gmail.com>
2021-03-28 19:46:42 -0700

    Added for loops

--- JS/app.js
+++ JS/app.js
@@ -3,4 +3,8 @@ let todolist = { todo: "clone repo for starter code", status:"complete" }
 
 let completedtodo = { }
 
+for ( let i of todolist ){
+    if (i === "complete")
+        completedtodo = {status:"complete"}
 
+}
